ee9c32675f7ed1e0b740b937c388bde621b0f017
Zhi Hong <56814471+tzhong0503@users.noreply.github.com>
2023-11-28 16:55:40 +0800

    Add files via upload

--- views/response.blade.php
+++ views/response.blade.php
@@ -1,28 +1,29 @@
-<!-- resources/views/payment.blade.php -->
-
 <html>
-<head>
-    <!-- Add any necessary styles or scripts -->
-</head>
-<body>
 
-    <!-- Render the payment form with the received data -->
-    <form id="paymentForm" action="https://stg-mpg.revpay-sandbox.com.my/v1/payment" method="post">
-        <!-- Add form fields with data from $responseData -->
-        <input type="text" name="Revpay_Merchant_ID" value="{{ $responseData['Revpay_Merchant_ID'] }}"><br>
+<head>
+    <meta charset="UTF-8">
+    <title>Redirecting to Revpay</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <form id="paymentForm" action="https://stg-mpg.revpay-sandbox.com.my/v1/payment" method="post" accept-charset="UTF-8">
         <input type="text" name="Merchant_Key" value="{{ $responseData['Merchant_Key'] }}"><br>
-        <input type="text" name="Key_Index" value="{{ $responseData['Key_Index'] }}"><br>
+        <input type="text" name="Revpay_Merchant_ID" value="{{ $responseData['Revpay_Merchant_ID'] }}"><br>
         <input type="text" name="Reference_Number" value="{{ $responseData['Reference_Number'] }}"><br>
         <input type="text" name="Amount" value="{{ $responseData['Amount'] }}"><br>
         <input type="text" name="Currency" value="{{ $responseData['Currency'] }}"><br>
         <input type="text" name="Transaction_Description" value="{{ $responseData['Transaction_Description'] }}"><br>
+        <input type="text" name="Payment_Type" value="{{ $responseData['Payment_Type'] }}"><br>
+        <input type="text" name="Key_Index" value="{{ $responseData['Key_Index'] }}"><br>
+        <input type="text" name="Return_URL" value="{{ $responseData['Return_URL'] }}"><br>
         <input type="text" name="Signature" value="{{ $responseData['Signature'] }}"><br>
     </form>
 
-    <!-- Add JavaScript to automatically submit the form -->
     <script>
         document.getElementById('paymentForm').submit();
     </script>
 
 </body>
+
 </html>
